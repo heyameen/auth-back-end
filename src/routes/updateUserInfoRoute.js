a5b14be05c6e68b00148db8dc5580c909165a599
bellhopAmeen <aladeameen@gmail.com>
2022-10-22 01:34:13 +0100

    fix on updatingUserInfo route

--- src/routes/updateUserInfoRoute.js
+++ src/routes/updateUserInfoRoute.js
@@ -1,54 +1,52 @@
-import { ObjectID } from 'mongodb'
-import { getDbConnection } from '../db'
-import jwt from 'jsonwebtoken'
+import { ObjectID } from 'mongodb';
+import { getDbConnection } from '../db';
+import jwt from 'jsonwebtoken';
 
 export const updateUserInfoRoute = {
     path: '/api/users/:userId',
     method: 'put',
     handler: async (req, res) => {
-        const { authorization } = req.headers
-        const { userId } = req.params
-    }
+        const { authorization } = req.headers;
+        const { userId } = req.params;
+        const updates = (({
+            favoriteFood,
+            hairColor,
+            bio,
+        }) => ({
+            favoriteFood,
+            hairColor,
+            bio,
+        }))(req.body); // A slick way of subsetting an object
 
-    const updates = ({
-        favoriteFood,
-        hairColor,
-        bio,
-    }) => ({
-        favoriteFood,
-        hairColor,
-        bio,
-        })(req.body)
-    
-    if(!authorization) {
-        return res.status(401).json({ message: 'No authorization header sent'})
-    }
+        if (!authorization) {
+            return res.status(401).json({ message: 'No authorization header sent' });
+        }
 
-    const token = authorization.split('')[1]
+        // Because authorization is "Bearer asdfkljasdfl.asdfasdf8as9f7a8s.asdfa89d7f89" - we need to remove "Bearer"
+        const [, token] = authorization.split(' ');
 
-    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => ({
-        if(err) return res.status(401).json({ message: 'Unable to verify token' })
-        
-        const { id } = decoded
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) return res.status(401).json({ message: 'Unable to verify token' });
+            
+            const { id, isVerified } = decoded;
 
-        if(id !== userId) return res.status(403).json({ message: 'Not allowed to update user\'s data' })
-    
-        const db = getDbConnection('react-auth-db')
-        const result = await db.collection('users').findOneAndUpdate(
-            { _id: ObjectID(Id) },
-            { $set: { info: updates } }
-            { returnOriginal: false}
-        )
-        
-        const { email, isVerified, info } = result.value
+            if (id !== userId) return res.status(403).json({ message: 'Not allowed to update that user\'s data' });
+            // if (!isVerified) return res.status(403).json({ message: 'You need to verify your email before you can update your data' });
 
-        jwt.sign({ id, email, isVerified, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
-            if (err) {
-                return res.status(200).json(err)
-            }
-
-            res.status(200).json({ token})
-        })
-
-    }))
-}
+            const db = getDbConnection('react-auth-db');
+            const result = await db.collection('users').findOneAndUpdate(
+                { _id: ObjectID(id) },
+                { $set: { info: updates } },
+                { returnOriginal: false },
+            ); 
+            const { email, info } = result.value;
+            
+            jwt.sign({ id, email, isVerified, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                res.status(200).json({ token });
+            });
+        });
+    },
+};
